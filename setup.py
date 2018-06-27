c21a891be92134f7abee736d0049698d42b379d6
Jure Varlec <jure@varlec.si>
2018-06-27 10:48:14 +0200

    setup.py: split out the libraries.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, Extension
 
 lib = Extension('poisson3d.support',
                 sources=['poisson3d/support.cpp'],
-                extra_compile_args=['--std=c++11',
-                                    '-fPIC', '-O2', '-lfftw3', '-DNDEBUG'],
+                libraries=['fftw3'],
+                extra_compile_args=['--std=c++11', '-fPIC', '-O2', '-DNDEBUG'],
                 )
 
 setup(name='poisson3d',
